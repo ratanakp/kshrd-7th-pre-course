ff61c2038e8b6a9a93c4b81520fac7fbdd87c5f0
Ratanak Phang <phangratanak168@gmail.com>
2018-12-26 16:56:32 +0700

    First Commit

---
+++ js/config.js
@@ -0,0 +1,34 @@
+// document.write('<h1>Hello from JavaScript</h1>')
+var x = document.getElementById('myDiv')
+
+var myVal = "this is new val", age = 21;
+var h2;
+
+var y = 7;
+
+var z = y + 2;
+
+var country = 'Cambodia';
+var country;
+
+age = 62.2;
+
+age = "fsdf";
+
+x.innerHTML = z;
+
+x.style.color = 'blue'; x.style.backgroundColor = 'red'
+
+document.getElementById("myImg").src = "mn-logo-yellow.png"
+
+// (function(){
+
+//     myFunction()
+// })();
+
+function myFunction () {
+    var u9999 = 'name';
+
+    alert('clicked my func!')
+    document.getElementById('myImg').src = 'iconfinder_Farmer_379478.png'
+}
